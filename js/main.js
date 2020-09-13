fbe0c53626f1fa8de20e768e80a6dbee5892618f
Sydney Pun <43591129+spun001@users.noreply.github.com>
2020-09-12 19:57:44 -0700

    added JS files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    $nav = $('.nav'); 
+    $toggleCollapse=$('.toggle-collapse');
+    
+    /* CLICK TOGGLE MENU */
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+})}); 
